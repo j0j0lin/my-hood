5dabf351977b3ec70026b4673c572c5125127a44
Jo-Jo Lin <jojolin@outlook.com>
2016-10-05 21:14:55 -0700

    -add managedObjc class files

---
+++ MyHood+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  MyHood+CoreDataClass.swift
+//  example-my-hood
+//
+//  Created by momo on 10/5/16.
+//  Copyright © 2016 Jo-Jo Lin. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class MyHood: NSManagedObject {
+
+}
